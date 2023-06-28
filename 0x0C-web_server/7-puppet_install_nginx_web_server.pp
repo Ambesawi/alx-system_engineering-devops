d5060d76dc3d9513db2e4bc7a70fafbdbf45ddc1
Ambesawi <ambesawi77@gmail.com>
2023-06-28 07:58:25 -0700

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,22 @@
-# Install nginx with puppet
+# Setup nginx server
+
 package { 'nginx':
-  ensure   => '1.18.0',
-  provider => 'apt',
+  ensure     => 'installed',
 }
 
-file { 'Hello World':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World',
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
 }
 
-file_line { 'Hello World':
-  path  => '/etc/nginx/sites-available/default',
-  after => 'server_name _;',
-  line  => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-exec { 'service':
-  command  => 'service nginx start',
-  provider => 'shell',
-  user     => 'root',
-  path     => '/usr/sbin/service',
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
+
